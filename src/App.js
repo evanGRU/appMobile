f128c3dc99f81790f6b0af4ba5a2359ef90dfa3d
evanGRU <evangruchot.pro@gmail.com>
2023-11-20 14:11:41 +0100

    add new font style and cloud API connection

--- src/App.js
+++ src/App.js
@@ -4,19 +4,43 @@ import axios from "axios";
 
 function App() {
     const [apiData, setApiData] = useState({});
-    const [powerData, setPowerData] = useState({});
-    useEffect(() => {
-        axios.get("http://192.168.1.100/relay/0").then((response) => {
-            setApiData(response.data);
-        });
-        axios.get("http://192.168.1.100/settings/status").then((response) => {
-            setPowerData(response.data);
+    const [powerState, setPowerState] = useState(false);
+    const id = "4022d88e30e8";
+    const auth_key = "MWNiMjY5dWlk404459961993DCA83AE44BC6E3A6F58906952E7BECA0A5B69DC375C964915ACBC0EA536A0639CB73";
+
+    useEffect( () => {
+        axios.get("https://shelly-77-eu.shelly.cloud/device/status?id="+id+"&auth_key="+auth_key).then(async (response) => {
+            await setApiData(response.data.data.device_status);
+            setPowerState(response.data.data.device_status.relays[0].ison);
         });
     }, [])
 
-    const [powerState, setPowerState] = useState(apiData.ison);
     const switchPowerState = () => {
-        axios.get("http://192.168.1.100/relay/0?turn="+ (powerState ? "on" : "off"));
+        const FormData = require('form-data');
+        let data = new FormData();
+        data.append('id', id);
+        data.append('turn', powerState ? 'off' : 'on');
+        data.append('auth_key', auth_key);
+        data.append('channel', '0');
+
+        const headers = data.getHeaders ? data.getHeaders() : { 'Content-Type': 'multipart/form-data' };
+        let config = {
+            method: 'post',
+            maxBodyLength: Infinity,
+            url: 'https://shelly-77-eu.shelly.cloud/device/relay/control',
+            headers: {
+                ...headers
+            },
+            data : data
+        };
+
+        axios.request(config)
+            .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch((error) => {
+                console.log(error);
+            });
         setPowerState(!powerState);
     };
 
@@ -31,9 +55,10 @@ function App() {
                     className="switchButton"
                 />
             </div>
-            <div>
+            <div className={'informations'}>
                 <h3>Informations : </h3>
-
+                <p>Overtemperature : {apiData.overtemperature ? 'Oui' : 'Non'}</p>
+                <p>Temperature : {apiData.temperature}</p>
             </div>
         </div>
     );
